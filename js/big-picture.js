dcbdf35ef9f9e5e748544aff29410393c5d18113
Danila Tkachenko <olrait9@gmail.com>
2024-07-26 11:39:39 +0200

    Поправил код после проверки наставника

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,3 +1,5 @@
+import { ESCAPE__KEY } from './utils';
+
 const bigPictureElement = document.querySelector('.big-picture');
 const commentCountElement = bigPictureElement.querySelector('.social__comment-count');
 const commentListElement = bigPictureElement.querySelector('.social__comments');
@@ -6,11 +8,9 @@ const bodyElement = document.querySelector('body');
 const cancelButtonElement = bigPictureElement.querySelector('.big-picture__cancel');
 const commentElement = document.querySelector('#comment').content.querySelector('.social__comment');
 
-const createComment = ({ avatar, name, message }) => {
+const createComment = ({ avatar, message }) => {
   const comment = commentElement.cloneNode(true);
-
   comment.querySelector('.social__picture').src = avatar;
-  comment.querySelector('.social__picture').alt = name;
   comment.querySelector('.social__text').textContent = message;
 
   return comment;
@@ -35,7 +35,7 @@ const hideBigPicture = () => {
 };
 
 function onDocumentKeydown(evt) {
-  if (evt.key === 'Escape') {
+  if (evt.key === ESCAPE__KEY) {
     evt.preventDefault();
     hideBigPicture();
   }
@@ -47,7 +47,6 @@ const onCancelButtonClick = () => {
 
 const renderPictureDetails = ({ url, likes, description }) => {
   bigPictureElement.querySelector('.big-picture__img img').src = url;
-  bigPictureElement.querySelector('.big-picture__img img').alt = description;
   bigPictureElement.querySelector('.likes-count').textContent = likes;
   bigPictureElement.querySelector('.social__caption').textContent = description;
 };
